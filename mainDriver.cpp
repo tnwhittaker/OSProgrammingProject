aeaa5c68c237aea9e380db238147ce0d62f8f81f
Torrike Whittaker <89610881+tnwhittaker@users.noreply.github.com>
2021-11-23 19:26:02 -0500

    Add files via upload

---
+++ mainDriver.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+void mainDriver(){
+    //Two Dimmensional array that stores the integer values and their key values(All are unlocked by default)
+    int sharedList[10][2]={{2,1},{4,1},{6,1},{8,1},{10,1},{12,1},{14,1},{16,1},{18,1},{20,1}};
+
+    cout<<"Hello, please enter ";
+    
+};
+
+int main(){
+    mainDriver();
+}
