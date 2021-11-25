758d0beeaf44e7c8f0592c1d85e21d59c7e6300a
CrypticHushane <oshanewilliams99@outlook.com>
2021-11-25 15:22:58 -0500

    added prompt and unfinished functionality

--- main.c
+++ main.c
@@ -2,10 +2,12 @@
 #include <stdlib.h>
 #include <time.h>
 
+void fcfs(int numberOfProcess, int startingPoint);
+
 typedef struct Process {
         int PID;
         int task;
-        int data;
+        int data;//should be an array
         int attempts;
         time_t start_time;
         time_t end_time;
@@ -13,8 +15,9 @@ typedef struct Process {
 
 int main(void)
 {
+    srand(time(0));
     int nP, startPoint;
-    int sharedList[10][2]={{2,1},{4,1},{6,1},{8,1},{10,1},{12,1},{14,1},{16,1},{18,1},{20,1}};
+    // int sharedList[10][2]={{2,1},{4,1},{6,1},{8,1},{10,1},{12,1},{14,1},{16,1},{18,1},{20,1}};
 
     printf("Enter the number of process(MINIMUM 10; MAXIMUM:30): ");
     scanf("%d", &nP);
@@ -22,6 +25,33 @@ int main(void)
     printf("Enter starting point(Must NOT BE GREATER THAN 10): ");
     scanf("%d", &startPoint);
 
+    fcfs(nP,startPoint);
     
     return 0;
+}
+
+void fcfs(int numberOfProcess, int startingPoint){
+    int sharedList[10][2]={{2,1},{4,1},{6,1},{8,1},{10,1},{12,1},{14,1},{16,1},{18,1},{20,1}};
+
+    int sharedListLength = (sizeof(sharedList) / sizeof(int))/2;
+
+    for (int i = 0; i < sharedListLength; i++)
+    {
+        int innerLength = (sizeof(sharedList[i]) / sizeof(int));
+        printf("%d ---- %d\n",sharedList[i][0],sharedList[i][1]);
+       
+        
+    }
+
+    for (int x = 0; x < numberOfProcess; x++)
+    {
+        Process process[x];
+        process->PID = rand() % 500;
+        process->task = (rand() % 3) + 1;//to be changed
+        
+        /* unfinished code */
+    }
+    
+    
+  
 }
