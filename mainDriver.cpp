7db3572e486e48ade967e4dd293b0bad5e7eac24
Torrike Whittaker <torrikedev@gmail.com>
2022-06-02 17:34:36 -0500

    Update

--- mainDriver.cpp
+++ mainDriver.cpp
@@ -5,7 +5,7 @@ void mainDriver(){
     //Two Dimmensional array that stores the integer values and their key values(All are unlocked by default)
     int sharedList[10][2]={{2,1},{4,1},{6,1},{8,1},{10,1},{12,1},{14,1},{16,1},{18,1},{20,1}};
 
-    cout<<"Hello, please enter ";
+    cout<<"Hello, please enter your name";
     
 };
 
